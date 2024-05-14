24853a4ae1d89fee8c60c4eb989fdf8ccbe0661f
Franco Griffo <98428536+francogriffo@users.noreply.github.com>
2024-05-14 20:58:37 -0300

    Add files via upload

---
+++ Club/clases/PagoActividad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Club.clases
+{
+    internal class PagoActividad : Pago
+    {
+        private int idActividad;
+
+        public PagoActividad(int idPago, int idCliente, DateTime fechaPago, double montoPagado, int idActividad)
+        : base(idPago, idCliente, fechaPago, montoPagado)
+        {
+            this.idActividad = idActividad;
+        }
+
+        public int IdActividad { get => idActividad; set => idActividad = value; }
+    }
+}
---
+++ Club/clases/Socio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Club.clases
+{
+    internal class Socio : Cliente
+    {
+
+        private int numeroSocio;
+        private int numeroCarnet;
+        private DateTime fechaAlta;
+
+        public Socio(string nombre, string apellido, string direccion, bool aptoFisico, int numeroSocio, int numeroCarnet, DateTime fechaAlta)
+            : base(nombre, apellido, direccion, aptoFisico)
+        {
+            this.numeroSocio = numeroSocio;
+            this.numeroCarnet = numeroCarnet;
+            this.fechaAlta = fechaAlta;
+        }
+
+        public int NumeroSocio { get => numeroSocio; set => numeroSocio = value; }
+        public int NumeroCarnet { get => numeroCarnet; set => numeroCarnet = value; }
+        public DateTime FechaAlta { get => fechaAlta; set => fechaAlta = value; }
+    }
+}
+    
